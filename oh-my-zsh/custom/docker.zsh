fe6176b88e2c931818243a06ad4d9dad8118959c
George Thomas <iamgeorgethomas@gmail.com>
2016-09-28 09:37:04 +0530

    Better docker aliases

--- oh-my-zsh/custom/docker.zsh
+++ oh-my-zsh/custom/docker.zsh
@@ -1,7 +1,8 @@
 # aliases
-alias docker-ehq="COMPOSE_HTTP_TIMEOUT=10000 docker-compose up"
-alias drmid="docker rmi $(docker images --filter "dangling=true" -q --no-trunc)"
-alias drmae='docker rm $(docker ps -qa --no-trunc --filter "status=exited")'
-alias drmv="docker volume rm $(docker volume ls -q -f 'dangling=true')"
-alias docker-bash="docker exec -it ehq_v2.unicorn_1 /bin/bash"
-alias docker-console="docker exec -it ehq_v2.unicorn_1 bundle exec rails c"
+alias dcup="COMPOSE_HTTP_TIMEOUT=10000 docker-compose up"
+alias drmi='docker rmi $(docker images --filter "dangling=true" -q --no-trunc)'
+alias drmc='docker rm $(docker ps -qa --no-trunc --filter "status=exited")'
+alias drmv='docker volume rm $(docker volume ls -q -f 'dangling=true')'
+alias dbash="docker exec -it ehq_v2.unicorn_1 /bin/bash"
+alias dconsole="docker exec -it ehq_v2.unicorn_1 bundle exec rails c"
+alias ddb="docker exec -it ehq_db_1 mysql"
