2b20b0224743ab2ece5d8365e517654da9b7d373
George Thomas <iamgeorgethomas@gmail.com>
2016-09-30 15:45:28 +0530

    Add alias for `docker-compose build`

--- oh-my-zsh/custom/docker.zsh
+++ oh-my-zsh/custom/docker.zsh
@@ -2,7 +2,8 @@
 alias dcup="COMPOSE_HTTP_TIMEOUT=10000 docker-compose up"
 alias drmi='docker rmi $(docker images --filter "dangling=true" -q --no-trunc)'
 alias drmc='docker rm $(docker ps -qa --no-trunc --filter "status=exited")'
-alias drmv='docker volume rm $(docker volume ls -q -f 'dangling=true')'
+alias drmv='docker volume rm $(docker volume ls -q -f "dangling=true")'
 alias dbash="docker exec -it ehq_v2.unicorn_1 /bin/bash"
 alias dconsole="docker exec -it ehq_v2.unicorn_1 bundle exec rails c"
 alias ddb="docker exec -it ehq_db_1 mysql"
+alias dcbuild="docker-compose build"
