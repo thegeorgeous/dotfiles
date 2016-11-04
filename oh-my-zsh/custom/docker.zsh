ec928c8faab63d79506aacf21e242371a2e58cb9
George Thomas <iamgeorgethomas@gmail.com>
2016-11-04 12:46:53 +0530

    Add more aliases for docker

--- oh-my-zsh/custom/docker.zsh
+++ oh-my-zsh/custom/docker.zsh
@@ -1,5 +1,5 @@
 # aliases
-alias dcup="COMPOSE_HTTP_TIMEOUT=10000 docker-compose up -d"
+alias dcup="COMPOSE_HTTP_TIMEOUT=10000 docker-compose up"
 alias drmi='docker rmi $(docker images --filter "dangling=true" -q --no-trunc)'
 alias drmc='docker rm $(docker ps -qa --no-trunc --filter "status=exited")'
 alias drmv='docker volume rm $(docker volume ls -q -f "dangling=true")'
@@ -8,3 +8,7 @@ alias dconsole="docker exec -it ehq_v2.unicorn_1 bundle exec rails c"
 alias ddb="docker exec -it ehq_db_1 mysql"
 alias dcbuild="docker-compose build"
 alias ddevlog="docker exec -it ehq_v2.unicorn_1 tail -f log/development.log"
+alias dclog="COMPOSE_HTTP_TIMEOUT=10000 docker-compose logs -f"
+alias dcrestart="docker-compose restart"
+alias dcs="docker-compose stop"
+alias dexit="docker exec -it"
