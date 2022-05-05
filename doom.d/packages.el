1b2dfeb7b56a9bbc428e65a11132bfffca5502c2
George Thomas <iamgeorgethomas@gmail.com>
2022-05-05 11:29:00 +0200

    Install jinja2 and coffee modes by default

--- doom.d/packages.el
+++ doom.d/packages.el
@@ -9,6 +9,8 @@
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
 (package! feature-mode)
+(package! jinja2-mode)
+(package! coffee-mode)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
