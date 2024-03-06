fb72c83b5efeab0e634e82e5bd3e627e74a9b807
George Thomas <iamgeorgethomas@gmail.com>
2024-03-06 09:52:05 +0100

    Set default theme doom pine

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-feather-dark)
+(setq doom-theme 'doom-pine)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -97,7 +97,10 @@
 (add-hook 'elixir-mode-hook
           (lambda () (add-hook 'before-save-hook 'elixir-format nil t)))
 
-(with-eval-after-load 'eglot
- (add-to-list 'eglot-server-programs '((ruby-mode ruby-ts-mode) "ruby-lsp")))
+;; (with-eval-after-load 'eglot
+;;  (add-to-list 'eglot-server-programs '((ruby-mode ruby-ts-mode) "ruby-lsp")))
 
 (setq doom-modeline-buffer-file-name-style 'truncate-with-project)
+
+(after! ruby-mode
+  (set-company-backend! 'ruby-mode))
